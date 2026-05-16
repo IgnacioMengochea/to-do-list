81d430f0b18e4b06633b43b59648975ab3ca19fe
ignaciomengocheadev <ignaciomengocheadev@gmail.com>
2026-05-16 20:22:05 -0300

    v2

--- app/task.php
+++ app/task.php
@@ -2,6 +2,8 @@
 
 function showTask(){
     require_once 'templates/header.php';
-    require_once 'templates/footer.php';
+    
     echo "Aca van todas las tareas...";
+
+    require_once 'templates/footer.php';
 }
