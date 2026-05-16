b5ca2d16cc04adbc4c24fd8ca8a016dcaba2249e
ignaciomengocheadev <ignaciomengocheadev@gmail.com>
2026-05-16 01:48:07 -0300

    Add .htaccess

--- router.php
+++ router.php
@@ -1,8 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 require_once 'app/task.php';
 
 define('BASE_URL', '//' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']) . '/');
