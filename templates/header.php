03e89b6817c6094e91ac770805c14032dcb6f489
ignaciomengocheadev <ignaciomengocheadev@gmail.com>
2026-05-18 01:09:56 -0300

    Add CSS

--- templates/header.php
+++ templates/header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <title>TUDAI - TODOList</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
 <div class="container">
